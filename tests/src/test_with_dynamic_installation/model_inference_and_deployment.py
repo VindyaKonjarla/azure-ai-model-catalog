4164511408329e500c0f0a4a6fb96ceb2e532595
SameepPanigrahi <v-sameepp@microsoft.com>
2023-09-21 17:03:27 +0530

    Adding code for dynamic installation

---
+++ tests/src/test_with_dynamic_installation/model_inference_and_deployment.py
@@ -0,0 +1,279 @@
+import time
+import json
+import os
+from azure.ai.ml.constants import AssetTypes
+from azure.ai.ml.entities import (
+    ManagedOnlineEndpoint,
+    ManagedOnlineDeployment,
+    OnlineRequestSettings,
+    ProbeSettings,
+    Model,
+    ModelConfiguration,
+    ModelPackage,
+    AzureMLOnlineInferencingServer
+)
+from utils.logging import get_logger
+from fetch_task import HfTask
+import mlflow
+from box import ConfigBox
+import re
+import sys
+
+logger = get_logger(__name__)
+
+
+class ModelInferenceAndDeployemnt:
+    def __init__(self, test_model_name, workspace_ml_client, registry) -> None:
+        self.test_model_name = test_model_name
+        self.workspace_ml_client = workspace_ml_client
+        self.registry = registry
+
+    def get_error_messages(self):
+        # load ../../config/errors.json into a dictionary
+        with open('../../config/errors.json') as f:
+            return json.load(f)
+
+    def prase_logs(self, logs):
+        error_messages = self.get_error_messages()
+        # split logs by \n
+        logs_list = logs.split("\n")
+        # loop through each line in logs_list
+        for line in logs_list:
+            # loop through each error in errors
+            for error in error_messages:
+                # if error is found in line, print error message
+                if error['parse_string'] in line:
+                    logger.error(
+                        f"::error:: {error_messages['error_category']}: {line}")
+
+    def get_online_endpoint_logs(self, deployment_name, online_endpoint_name):
+        logger.info("Deployment logs: \n\n")
+        logs = self.workspace_ml_client.online_deployments.get_logs(
+            name=deployment_name, endpoint_name=online_endpoint_name, lines=100000)
+        print(logs)
+        self.prase_logs(logs)
+
+    def get_latest_model_version(self, workspace_ml_client, model_name):
+        logger.info("In get_latest_model_version...")
+        version_list = list(workspace_ml_client.models.list(model_name))
+        if len(version_list) == 0:
+            logger.info("Model not found in registry")
+        else:
+            model_version = version_list[0].version
+            foundation_model = workspace_ml_client.models.get(
+                model_name, model_version)
+            logger.info(
+                "\n\nUsing model name: {0}, version: {1}, id: {2} for inferencing".format(
+                    foundation_model.name, foundation_model.version, foundation_model.id
+                )
+            )
+        logger.info(
+            f"Latest model {foundation_model.name} version {foundation_model.version} created at {foundation_model.creation_context.created_at}")
+        #print(f"Model Config : {latest_model.config}")
+        return foundation_model
+
+    def cloud_inference(self, scoring_file, scoring_input, online_endpoint_name, deployment_name):
+        try:
+            logger.info(f"endpoint_name : {online_endpoint_name}")
+            logger.info(f"deployment_name : {deployment_name}")
+            logger.info(f"Input data is this one : {scoring_input}")
+            response = self.workspace_ml_client.online_endpoints.invoke(
+                endpoint_name=online_endpoint_name,
+                deployment_name=deployment_name,
+                request_file=scoring_file,
+            )
+            response_json = json.loads(response)
+            output = json.dumps(response_json, indent=2)
+            logger.info(f"response: \n\n{output}")
+            with open(os.environ['GITHUB_STEP_SUMMARY'], 'a') as fh:
+                logger.info(f'####Sample input', file=fh)
+                logger.info(f'```json', file=fh)
+                logger.info(f'{scoring_input}', file=fh)
+                logger.info(f'```', file=fh)
+                logger.info(f'####Sample output', file=fh)
+                logger.info(f'```json', file=fh)
+                logger.info(f'{output}', file=fh)
+                logger.info(f'```', file=fh)
+        except Exception as e:
+            logger.error(f"::error:: Could not invoke endpoint: \n")
+            logger.info(f"{e}\n\n check logs:\n\n")
+
+    def create_online_endpoint(self, endpoint):
+        logger.info("In create_online_endpoint...")
+        try:
+            self.workspace_ml_client.online_endpoints.begin_create_or_update(
+                endpoint).wait()
+        except Exception as e:
+            logger.error(f"::error:: Could not create endpoint: \n")
+            logger.error(f"{e}\n\n check logs:\n\n")
+            self.prase_logs(str(e))
+            exit(1)
+        online_endpoint_obj = self.workspace_ml_client.online_endpoints.get(
+            name=endpoint.name)
+        logger.info(f"online_endpoint_obj : {online_endpoint_obj}")
+
+    def get_model_name(self, latest_model_name):
+        # Expression need to be replaced with hyphen
+        expression_to_ignore = ["/", "\\", "|", "@", "#", ".",
+                                "$", "%", "^", "&", "*", "<", ">", "?", "!", "~", "_"]
+        # Create the regular expression to ignore
+        regx_for_expression = re.compile(
+            '|'.join(map(re.escape, expression_to_ignore)))
+        # Check the model_name contains any of there character
+        expression_check = re.findall(regx_for_expression, latest_model_name)
+        if expression_check:
+            # Replace the expression with hyphen
+            latest_model_name = regx_for_expression.sub("-", latest_model_name)
+        # Reserve Keyword need to be removed
+        reserve_keywords = ["microsoft"]
+        # Create the regular expression to ignore
+        regx_for_reserve_keyword = re.compile(
+            '|'.join(map(re.escape, reserve_keywords)))
+        # Check the model_name contains any of the string
+        reserve_keywords_check = re.findall(
+            regx_for_reserve_keyword, latest_model_name)
+        if reserve_keywords_check:
+            # Replace the resenve keyword with nothing with hyphen
+            latest_model_name = regx_for_reserve_keyword.sub(
+                '', latest_model_name)
+            latest_model_name = latest_model_name.lstrip("-")
+
+        return latest_model_name
+
+    def create_online_deployment(self, latest_model, online_endpoint_name, model_package, instance_type, endpoint):
+        logger.info("In create_online_deployment...")
+        logger.info(f"latest_model.name is this : {latest_model.name}")
+        latest_model_name = self.get_model_name(
+            latest_model_name=latest_model.name)
+        # Check if the model name starts with a digit
+        if latest_model_name[0].isdigit():
+            num_pattern = "[0-9]"
+            latest_model_name = re.sub(num_pattern, '', latest_model_name)
+            latest_model_name = latest_model_name.strip("-")
+        # Check the model name is more then 32 character
+        if len(latest_model.name) > 32:
+            model_name = latest_model_name[:31]
+            deployment_name = model_name.rstrip("-")
+        else:
+            deployment_name = latest_model_name
+        logger.info(f"deployment name is this one : {deployment_name}")
+        deployment_config = ManagedOnlineDeployment(
+            name=deployment_name,
+            model=latest_model.id,
+            endpoint_name=online_endpoint_name,
+            instance_type=instance_type,
+            instance_count=1,
+            request_settings=OnlineRequestSettings(
+                max_concurrent_requests_per_instance=1,
+                request_timeout_ms=50000,
+                max_queue_wait_ms=500,
+            )
+        )
+        try:
+            self.workspace_ml_client.online_deployments.begin_create_or_update(
+                deployment_config).wait()
+        except Exception as e:
+            _, _, exc_tb = sys.exc_info()
+            logger.error(f"::error:: Could not create deployment\n")
+            logger.error(f"The exception occured at this line no : {exc_tb.tb_lineno}" +
+                         f" the exception is this one : {e}")
+            self.prase_logs(str(e))
+            self.get_online_endpoint_logs(
+                deployment_name, online_endpoint_name)
+            self.workspace_ml_client.online_endpoints.begin_delete(
+                name=online_endpoint_name).wait()
+            exit(1)
+        endpoint.traffic = {deployment_name: 100}
+        try:
+            self.workspace_ml_client.begin_create_or_update(endpoint).result()
+        except Exception as e:
+            _, _, exc_tb = sys.exc_info()
+            logger.error(f"::error:: Could not create deployment\n")
+            logger.error(f"The exception occured at this line no : {exc_tb.tb_lineno}" +
+                         f" the exception is this one : {e}")
+            self.get_online_endpoint_logs(
+                deployment_name, online_endpoint_name)
+            self.workspace_ml_client.online_endpoints.begin_delete(
+                name=endpoint.name).wait()
+            exit(1)
+        deployment_obj = self.workspace_ml_client.online_deployments.get(
+            name=deployment_name, endpoint_name=endpoint.name)
+        logger.info(f"Deployment object is this one: {deployment_obj}")
+        return deployment_name
+
+    def delete_online_endpoint(self, online_endpoint_name):
+        try:
+            logger.info("\n In delete_online_endpoint.....")
+            self.workspace_ml_client.online_endpoints.begin_delete(
+                name=online_endpoint_name).wait()
+        except Exception as e:
+            _, _, exc_tb = sys.exc_info()
+            logger.error(f"The exception occured at this line no : {exc_tb.tb_lineno}" +
+                         f" the exception is this one : {e}")
+            logger.error(f"::warning:: Could not delete endpoint: : \n{e}")
+            exit(0)
+
+    def get_task_specified_input(self, task):
+        scoring_file = f"../../config/sample_inputs/{self.registry}/{task}.json"
+        #scoring_file = f"sample_inputs/{task}.json"
+        # check of scoring_file exists
+        try:
+            with open(scoring_file) as f:
+                scoring_input = ConfigBox(json.load(f))
+                logger.info(f"scoring_input file:\n\n {scoring_input}\n\n")
+        except Exception as e:
+            logger.warning(
+                f"::warning:: Could not find scoring_file: {scoring_file}. Finishing without sample scoring: \n{e}")
+        return scoring_file, scoring_input
+
+    def model_infernce_and_deployment(self, instance_type):
+        expression_to_ignore = ["/", "\\", "|", "@", "#", ".",
+                                "$", "%", "^", "&", "*", "<", ">", "?", "!", "~"]
+        # Create the regular expression to ignore
+        regx_for_expression = re.compile(
+            '|'.join(map(re.escape, expression_to_ignore)))
+        # Check the model_name contains any of there character
+        expression_check = re.findall(
+            regx_for_expression, self.test_model_name)
+        if expression_check:
+            # Replace the expression with hyphen
+            model_name = regx_for_expression.sub("-", self.test_model_name)
+        else:
+            model_name = self.test_model_name
+        latest_model = self.get_latest_model_version(
+            self.workspace_ml_client, model_name)
+        try:
+            task = latest_model.flavors["transformers"]["task"]
+        except Exception as e:
+            logger.warning(
+                f"::warning::From the transformer flavour we are not able to extract the task for this model : {latest_model}")
+            logger.info(f"Following Alternate approach to getch task....")
+            hfApi = HfTask(model_name=self.test_model_name)
+            task = hfApi.get_task()
+        logger.info(f"latest_model: {latest_model}")
+        logger.info(f"Task is : {task}")
+        scoring_file, scoring_input = self.get_task_specified_input(task=task)
+        # endpoint names need to be unique in a region, hence using timestamp to create unique endpoint name
+        timestamp = int(time.time())
+        online_endpoint_name = task + str(timestamp)
+        #online_endpoint_name = "Testing" + str(timestamp)
+        logger.info(f"online_endpoint_name: {online_endpoint_name}")
+        endpoint = ManagedOnlineEndpoint(
+            name=online_endpoint_name,
+            auth_mode="key",
+        )
+        self.create_online_endpoint(endpoint=endpoint)
+        deployment_name = self.create_online_deployment(
+            latest_model=latest_model,
+            online_endpoint_name=online_endpoint_name,
+            model_package=" ",
+            instance_type=instance_type,
+            endpoint=endpoint
+        )
+        # self.cloud_inference(
+        #     scoring_file=scoring_file,
+        #     scoring_input=scoring_input,
+        #     online_endpoint_name=online_endpoint_name,
+        #     deployment_name=deployment_name
+        # )
+        self.delete_online_endpoint(online_endpoint_name=online_endpoint_name)
