e0e43e08eb0252ed4f58ea18bc5cf353053a417b
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2023-09-26 17:20:12 +0530

    Update create_queue.py

--- tests/setup/create_queue.py
+++ tests/setup/create_queue.py
@@ -116,7 +116,7 @@ def assign_models_to_queues(models, workspace_list):
                         print("queue[workspace]",queue[workspace])
                     if thread not in queue[workspace]:
                         queue[workspace][thread] = []
-                    queue[workspace][thread].append("MLFlow-MP-"+models[i])
+                    queue[workspace][thread].append("MLFlow-DI-"+models[i])
                     print("queue[workspace][thread]",queue[workspace][thread])
                     i=i+1
                     #print (f"Adding model {models[i]} at index {i} to queue {workspace}-{thread}")
@@ -184,11 +184,9 @@ def write_single_workflow_file(model, q, secret_name):
     # # print(workflow_file['env']['test_queue'])
     print (f"Generating workflow file: {workflow_file}")
     os.system(f"cp {args.workflow_template} {workflow_file}")
-    test_Model=model.replace("MLFlow-MP-"," ")
+    test_Model=model.replace("MLFlow-DI-"," ")
     test_model_name=test_Model.strip()
     print(test_model_name)
-    # MLmodel="MLFlow-MP-"+model
-    # print("MLmodel:",model)
     os.system(f"sed -i s/name: .*/name: {model}/g' {workflow_file}")
     # replace <test_queue> with q
     os.system(f"sed -i 's/test_queue: .*/test_queue: {q}/g' {workflow_file}")
