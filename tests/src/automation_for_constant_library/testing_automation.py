665a3cecaad183660bfc1779b11161880b02b7a9
amarahiqbal <141737528+amarahiqbal@users.noreply.github.com>
2023-09-29 15:29:27 +0530

    Update testing_automation.py

--- tests/src/automation_for_constant_library/testing_automation.py
+++ tests/src/automation_for_constant_library/testing_automation.py
@@ -185,7 +185,7 @@ if __name__ == "__main__":
     latest_env = workspace_ml_client.environments.get(
         name=queue.environment, version=str(latest_version))
     logger.info(f"Latest Environment : {latest_env}")
-    command_job = run_azure_ml_job(code="./", command_to_run="python generic_model_download_and_register.py",
+    command_job = run_azure_ml_job(code="./", command_to_run="python testing_reg.py",
                                    environment=latest_env, compute=queue.compute, environment_variables=environment_variables)
     create_and_get_job_studio_url(command_job, workspace_ml_client)
 
