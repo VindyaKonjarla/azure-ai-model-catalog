f50e4d8a87660fb0e86833db05f309dfaff77a1e
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2023-10-11 18:31:40 +0530

    Update Import_Register.py

--- tests/src/pipeline_component/Import_Register.py
+++ tests/src/pipeline_component/Import_Register.py
@@ -242,7 +242,7 @@ if __name__ == "__main__":
         # print(f"\n{model_name}")
         print(model.__dict__)
         flavour = model.flavors
-        print("flavour:----",flavour)
+        print("flavour:----",model.flavors.python_function.loader_module)
         if flavour== None:
             print(f"This model {model.name} is not registered in the mlflow flavour")
             raise Exception('Some message')
