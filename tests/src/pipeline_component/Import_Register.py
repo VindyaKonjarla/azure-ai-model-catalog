67bee0a0de11122e74db71e95e151015da10e184
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2023-10-11 17:23:23 +0530

    Update Import_Register.py

--- tests/src/pipeline_component/Import_Register.py
+++ tests/src/pipeline_component/Import_Register.py
@@ -92,7 +92,7 @@ def set_next_trigger_model(queue):
     model_list = list(queue.models)
     #model_name_without_slash = test_model_name.replace('/', '-')
     # check_mlflow_model = "MLFlow-Import-"+test_model_name
-    check_mlflow_model = "IM-Flavourcheck--"+test_model_name
+    check_mlflow_model = "IM-Flavourcheck-"+test_model_name
     index = model_list.index(check_mlflow_model)
     #index = model_list.index(test_model_name)
     print(f"index of {test_model_name} in queue: {index}")
