e38f067a0440a7d41a31855af5fee6200863b705
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2023-10-13 13:52:42 +0530

    Update create_queue.py

--- tests/setup/create_queue.py
+++ tests/setup/create_queue.py
@@ -116,7 +116,7 @@ def assign_models_to_queues(models, workspace_list):
                         print("queue[workspace]",queue[workspace])
                     if thread not in queue[workspace]:
                         queue[workspace][thread] = []
-                    queue[workspace][thread].append("MLFlow-Evaluate-"+models[i])
+                    queue[workspace][thread].append("MLFlow-Import-"+models[i])
                     print("queue[workspace][thread]",queue[workspace][thread])
                     i=i+1
                     #print (f"Adding model {models[i]} at index {i} to queue {workspace}-{thread}")
