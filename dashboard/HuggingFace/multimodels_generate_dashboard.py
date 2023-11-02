13a10b50af328ae93ae98d51f8ab4ffdfd796ddb
p9523 <141737115+p9523@users.noreply.github.com>
2023-11-02 17:23:05 +0530

    Add files via upload

---
+++ dashboard/HuggingFace/multimodels_generate_dashboard.py
@@ -0,0 +1,49 @@
+dashboard_content = """
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            text-align: center;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+
+<table>
+    <tr>
+        <th>Model Name</th>
+        <th>Status</th>
+    </tr>
+    <tr>
+        <td>Model 1</td>
+        <td>In Progress</td>
+    </tr>
+    <tr>
+        <td>Model 2</td>
+        <td>Passed</td>
+    </tr>
+    <tr>
+        <td>Model 3</td>
+        <td>Failed</td>
+    </tr>
+</table>
+
+</body>
+</html>
+"""
+
+# Save the generated HTML content to a file
+with open('multimodels_dashboard.html', 'w') as file:
+    file.write(dashboard_content)
