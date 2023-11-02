46ea01079d071fbff5c8f5054e577b2a3e224fed
PoojaRam-12 <141908731+PoojaRam-12@users.noreply.github.com>
2023-11-02 16:06:22 +0530

    Update Batch_Endpoint_FT.py

--- tests/src/Batch_Endpoint/Batch_Endpoint_FT.py
+++ tests/src/Batch_Endpoint/Batch_Endpoint_FT.py
@@ -1,16 +1,19 @@
 from transformers import AutoTokenizer
-from azureml.core import Workspace
+from azureml.core import Workspace, Environment
+#from batch_inference_and_deployment import BatchDeployemnt
 from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential
-from azure.ai.ml.entities import AmlCompute, AmlCompute, BatchDeployment, BatchRetrySettings
+from azure.ai.ml.entities import AmlCompute
+from azure.ai.ml import command
 from azure.ai.ml import Input, MLClient
+import mlflow
 import json
 import os
-import re
-import time
-from fetch_tasks import HfTask
-from azure.ai.ml.entities import BatchEndpoint
-from azureml.core.datastore import Datastore
-from azureml.core import Workspace
+import sys
+from box import ConfigBox
+from azureml.core.compute import AmlCompute
+from huggingface_hub import login
+from azureml.core.compute_target import ComputeTargetException
+from azure.ai.ml.constants import AssetTypes
 from mlflow.tracking.client import MlflowClient
 import time
 from fetch_tasks import HfTask
@@ -26,7 +29,6 @@ from azureml.core import Workspace
 from mlflow.tracking.client import MlflowClient
 import re
 from datetime import datetime
-
 
 ACCESS_TOKEN = "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"
 
