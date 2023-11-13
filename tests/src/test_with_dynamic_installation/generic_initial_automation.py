24cb3fec3af967b9f4b2856ef90031f93918991e
SameepPanigrahi <v-sameepp@microsoft.com>
2023-11-13 15:42:06 +0530

    Correcting naming convention

--- tests/src/test_with_dynamic_installation/generic_initial_automation.py
+++ tests/src/test_with_dynamic_installation/generic_initial_automation.py
@@ -189,8 +189,8 @@ if __name__ == "__main__":
     # command_job = run_azure_ml_job(code="./", command_to_run="python generic_model_download_and_register.py",
     #                                environment=latest_env, compute=queue.compute, environment_variables=environment_variables)
     # create_and_get_job_studio_url(command_job, workspace_ml_client)
-
-    foundation_model = ModelDetail(workspace_ml_client=workspace_ml_client).get_model_detail(test_model_name=test_model_name)
+    registered_model_name = test_model_name.replace("/", "-").lower()
+    foundation_model = ModelDetail(workspace_ml_client=workspace_ml_client).get_model_detail(test_model_name=registered_model_name)
     InferenceAndDeployment = ModelInferenceAndDeployemnt(
         test_model_name=test_model_name.lower(),
         workspace_ml_client=workspace_ml_client,
