90bfd0525ba2dac72e26822947e77fb699af5c1b
PoojaRam-12 <141908731+PoojaRam-12@users.noreply.github.com>
2023-11-20 17:00:57 +0530

    Update Batch_Endpoint_FT.py

--- tests/src/Batch_Endpoint/Batch_Endpoint_FT.py
+++ tests/src/Batch_Endpoint/Batch_Endpoint_FT.py
@@ -421,30 +421,30 @@ if __name__ == "__main__":
     task_mapping = {
         "text-classification": {
             "token-classification": "FT-NER-"+str(test_model_name)+"-oss",
-            "question-answering": "FT-QA-{test_model_name}"
+            "question-answering": "FT-QA-str(test_model_name)+"-oss"
         },
         "fill-mask": {
             "question-answering": "FT-QA-"+str(test_model_name)+"-oss"
         },
         "text-generation": {
-            "text-classification": "FT-TC-{test_model_name}-oss",
-            "token-classification": "FT-NER-{test_model_name}-oss"
+            "text-classification": "FT-TC-str(test_model_name)+"-oss"",
+            "token-classification": "FT-NER-str(test_model_name)+"-oss""
         },
         "question-answering": {
-            "text-classification": "FT-TC-{test_model_name}-oss",
-            "question-answering": "FT-QA-{test_model_name}-oss"
+            "text-classification": "FT-TC-str(test_model_name)+"-oss"",
+            "question-answering": "FT-QA-str(test_model_name)+"-oss""
         },
         "translation": {
-            "summarization": "FT-TS-{test_model_name}-oss",
-            "translation": "FT-TT-{test_model_name}-oss"
+            "summarization": "FT-TS-str(test_model_name)+"-oss"",
+            "translation": "FT-TT-str(test_model_name)+"-oss""
         },
         "summarization": {
-            "summarization": "FT-TS-{test_model_name}-oss",
-            "translation": "FT-TT-{test_model_name}-oss"
+            "summarization": "FT-TS-str(test_model_name)+"-oss"",
+            "translation": "FT-TT-str(test_model_name)+"-oss""
         },
         "token-classification": {
-            "text-classification": "FT-TC-{test_model_name}-oss",
-            "token-classification": "FT-NER-{test_model_name}-oss"
+            "text-classification": "FT-TC-str(test_model_name)+"-oss"",
+            "token-classification": "FT-NER-str(test_model_name)+"-oss""
         }
     }
 
