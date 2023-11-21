89bae2f3d6ba629c6ac61240f9d248985cd2a63b
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2023-11-21 17:19:45 +0530

    Update test.py

--- tests/src/pipeline_component_automation/test.py
+++ tests/src/pipeline_component_automation/test.py
@@ -339,14 +339,15 @@ if __name__ == "__main__":
     # -----------------------------------------
     registered_model_detail = ModelDetail(workspace_ml_client=workspace_ml_client)
     registered_model = registered_model_detail.get_model_detail(test_model_name=test_model_name)
-    try:
-      mlflow_version = registered_model.mlflow_version
-      flavour = registered_model.flavors
-      transformers_version=flavour.get("transformers").get("transformers_version", None)
-      tv=flavour.get("hftransformersv2").get("transformers_version", None)
+    mlflow_version = registered_model.mlflow_version
+    flavour = registered_model.flavors
+    transformers_version=flavour.get("transformers").get("transformers_version", None)
+    tv=flavour.get("hftransformersv2").get("transformers_version", None)
       # transformersversion=transformers_version==4.34.0 or tv==4.34.0  
-      print("mlflow_version",mlflow_version)
-      print("tv",tv)
+    print("mlflow_version",mlflow_version)
+    print("tv",tv)
+    try:
+     
       if mlflow_version==2.8.0 and tv==4.34.0:
         logger.info(
                 f"This model {registered_model.name} is in mlflow_version==2.8.0 && transformers_version==4.34.0")
