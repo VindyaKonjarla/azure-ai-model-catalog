10746af8461ea175b7aedb31f204e0413487e6cb
PoojaRam-12 <141908731+PoojaRam-12@users.noreply.github.com>
2023-11-22 16:42:22 +0530

    Update FT_P_NER.py

--- tests/src/FT_PC/FT_P_NER.py
+++ tests/src/FT_PC/FT_P_NER.py
@@ -329,7 +329,9 @@ if __name__ == "__main__":
     )
     mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
     registry_ml_client = MLClient(credential, registry_name="azureml-preview-test1")
-    experiment_name = "token-classification-ner"
+    # experiment_name = "token-classification-ner"
+    experiment_name = "token-classification-ner-"+ test_model_name
+    print("Experiment name is:", {experiment_name})
     # # generating a unique timestamp that can be used for names and versions that need to be unique
     # timestamp = str(int(time.time()))
 
