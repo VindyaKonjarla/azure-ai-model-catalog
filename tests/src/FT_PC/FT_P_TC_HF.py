4c9ac2d631a755b14dd1f15db8120d680d1858b1
PoojaRam-12 <141908731+PoojaRam-12@users.noreply.github.com>
2023-11-30 18:31:32 +0530

    Update FT_P_TC_HF.py

--- tests/src/FT_PC/FT_P_TC_HF.py
+++ tests/src/FT_PC/FT_P_TC_HF.py
@@ -1,4 +1,3 @@
-#from model_inference_and_deployment import ModelInferenceAndDeployemnt
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, Seq2SeqTrainingArguments, Trainer, DataCollatorForSeq2Seq, TrainingArguments
 from azure.ai.ml import command
 import mlflow
@@ -6,9 +5,11 @@ import json
 import os
 import sys
 from box import ConfigBox
+from azure.ai.ml.entities import Model
 from mlflow.tracking.client import MlflowClient
 from azureml.core import Workspace, Environment
 from azure.ai.ml import MLClient
+from azureml.core import Model
 from azure.identity import (
     DefaultAzureCredential,
     InteractiveBrowserCredential
@@ -20,6 +21,11 @@ from azure.ai.ml.entities import CommandComponent, PipelineComponent, Job, Compo
 from azure.ai.ml import PyTorchDistribution, Input
 import ast
 import re
+from datetime import datetime
+import time
+from azureml.core import Model
+from azure.ai.ml.entities import Model
+
 # from azure.ai.ml.entities import MLClient
 
 check_override = True
@@ -367,6 +373,37 @@ def create_and_run_azure_ml_pipeline(
         name="text_classification_pipeline_for_oss", label="latest"
     )
 
+
+    def register_model_to_workspace(
+        workspace_ml_client, pipeline_job, test_model_name, timestamp
+    ):
+        print("Registering the model inside loop...")
+        model_path_from_job = "azureml://jobs/{0}/outputs/{1}".format(
+            pipeline_job.name, "trained_model"
+        )
+        finetuned_model_name = (
+            "FT-TC-" + str(test_model_name) + "-oss"
+        )
+        finetuned_model_name = finetuned_model_name.replace("/", "-")
+        print("The Finetuned model name inside loop:", finetuned_model_name)
+
+        print("Path to register model inside loop: ", model_path_from_job)
+
+        prepare_to_register_model = Model(
+        path=model_path_from_job,
+        type=AssetTypes.MLFLOW_MODEL,
+        name=finetuned_model_name,
+        version=timestamp,  # use timestamp as version to avoid version conflict
+        description= test_model_name + " fine tuned model for emotion detection",
+        )
+        print("prepare to register model inside loop:", prepare_to_register_model)
+    
+        registered_model = workspace_ml_client.models.create_or_update(
+            prepare_to_register_model
+        )
+    
+        print("Registered model inside loop: \n", registered_model)
+
     # Define the pipeline job
     @pipeline()
     def create_pipeline():
@@ -412,6 +449,12 @@ def create_and_run_azure_ml_pipeline(
 
     # Wait for the pipeline job to complete
     workspace_ml_client.jobs.stream(pipeline_job.name)
+    print("Pipeline Job Completed")
+
+    # Call the model registration function
+    register_model_to_workspace(
+        workspace_ml_client, pipeline_job, test_model_name, timestamp
+    )
     return pipeline_job
 
 
@@ -463,18 +506,24 @@ if __name__ == "__main__":
     mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
     registry_ml_client = MLClient(credential, registry_name="azureml-preview-test1")
     registry_ml_client_model = MLClient(credential, registry_name="azureml")
-    experiment_name = "text-classification-emotion-detection-"+ test_model_name
+    
+    experiment_name = "hf-text-classification-"+ test_model_name
     print("Experiment name is:", {experiment_name})
 
     # # generating a unique timestamp that can be used for names and versions that need to be unique
     # timestamp = str(int(time.time()))
 
-    # Define the compute cluster name and size
-    compute_cluster = "Standard-NC24s-v3"
-    compute_cluster_size = "Standard_NC24s_v3 "
+    # # Define the compute cluster name and size
+    # compute_cluster = "Standard-NC24s-v3"
+    # compute_cluster_size = "Standard_NC24s_v3 "
+
+    compute_cluster_size = fine_tune_sku
+    compute_cluster = compute_cluster_size.replace('_', '-')
+    print("Modified compute_cluster_size:", compute_cluster_size)
+    print("Modified compute_cluster_size:", {compute_cluster})
     
     # Optional: Define a list of allowed compute sizes (if any)
-    computes_allow_list = ["standard_nc6s_v3", "standard_nc12s_v2","standard_nc24s_v3"]
+    computes_allow_list = ["standard_nc6s_v3", "standard_nc12s_v2","standard_nc24s_v3","standard_NC24rs_v3","Standard_NC12s_v3"]
     
     # Call the function
     compute, gpus_per_node, compute_cluster = create_or_get_aml_compute(workspace_ml_client, compute_cluster, compute_cluster_size, computes_allow_list)
@@ -512,23 +561,32 @@ if __name__ == "__main__":
     
     #foundation_model, foundation_model_name = get_latest_model_version(workspace_ml_client, test_model_name.lower())
     foundation_model = get_latest_model_version(registry_ml_client_model, test_model_name.lower())
+    fine_tune_sku = foundation_model_ft.properties.get("finetune-recommended-sku")
+    print("Finetune-recommended-sku:", {fine_tune_sku})
     training_parameters, optimization_parameters = get_training_and_optimization_parameters(foundation_model)
     #gpus_per_node = find_gpus_in_compute(workspace_ml_client, compute)
     print(f"Number of GPUs in compute: {gpus_per_node}")
+
+
+    
+    timestamp_str = str(time.time())
+    timestamp = timestamp_str.split(".")[0]
+    print("timestamp_str:",{timestamp_str})
+    print("timestamp:",{timestamp})
 
 
     try:
         pipeline_job = create_and_run_azure_ml_pipeline(
             foundation_model, compute_cluster, gpus_per_node, training_parameters, optimization_parameters, experiment_name
         )
-        print("Azure ML Pipeline completed successfully.")
+        print("Pipeline job completed")
     except Exception as e:
         # If an exception occurs, print the error message and exit with a non-zero exit code
         print(f"Error running Azure ML Pipeline: {str(e)}")
         sys.exit(1) 
 
     #pipeline_job = create_and_run_azure_ml_pipeline(foundation_model, compute_cluster, gpus_per_node, training_parameters, optimization_parameters, experiment_name)
-    print("Completed")
+    print("Finetuned and the registered model for Text-classification successfully")
 
     
     
