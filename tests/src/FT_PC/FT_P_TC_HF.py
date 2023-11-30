608ca7c985de5c1cb1a0f95d21c5285e4b37751d
PoojaRam-12 <141908731+PoojaRam-12@users.noreply.github.com>
2023-11-30 21:38:45 +0530

    Update FT_P_TC_HF.py

--- tests/src/FT_PC/FT_P_TC_HF.py
+++ tests/src/FT_PC/FT_P_TC_HF.py
@@ -382,7 +382,7 @@ def create_and_run_azure_ml_pipeline(
             pipeline_job.name, "trained_model"
         )
         finetuned_model_name = (
-            "FT-TC-" + str(test_model_name) + "-oss"
+            "FT-TC-" + str(test_model_name) + "-hf"
         )
         finetuned_model_name = finetuned_model_name.replace("/", "-")
         print("The Finetuned model name inside loop:", finetuned_model_name)
