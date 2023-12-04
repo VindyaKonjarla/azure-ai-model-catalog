a58fafd1576379aac6332b15c58851e5f7b61b52
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2023-12-04 14:19:37 +0530

    Update generic_evaluation_automation.py

--- tests/src/pipeline_component_automation/generic_evaluation_automation.py
+++ tests/src/pipeline_component_automation/generic_evaluation_automation.py
@@ -81,8 +81,8 @@ def set_next_trigger_model(queue):
 # file the index of test_model_name in models list queue dictionary
     model_list = list(queue.models)
     #model_name_without_slash = test_model_name.replace('/', '-')
-    check_mlflow_model = "MLFlow-"+test_model_name
-    import_alias_model_name = f"MLFlow-Eval-{test_model_name}"
+    check_mlflow_model = "oss-"+test_model_name
+    import_alias_model_name = f"oss-base-{test_model_name}"
 
     if check_mlflow_model in model_list:
         index = model_list.index(check_mlflow_model)
