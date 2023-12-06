20bbf3bf0c8a343e812e5fa45b4b872d56cddcde
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2023-12-06 16:30:38 +0530

    Update generic_eval.py

--- tests/src/pipeline_component_automation/generic_eval.py
+++ tests/src/pipeline_component_automation/generic_eval.py
@@ -207,161 +207,154 @@ def evaluation_pipeline(task, mlflow_model, test_data, input_column_names, label
 
 if __name__ == "__main__":
     # if any of the above are not set, exit with error
-    if test_model_name is None or test_sku_type is None or test_queue is None or test_set is None or test_trigger_next_model is None or test_keep_looping is None:
-        logger.error("::error:: One or more of the environment variables test_model_name, test_sku_type, test_queue, test_set, test_trigger_next_model, test_keep_looping are not set")
-        exit(1)
+    # if test_model_name is None or test_sku_type is None or test_queue is None or test_set is None or test_trigger_next_model is None or test_keep_looping is None:
+    #     logger.error("::error:: One or more of the environment variables test_model_name, test_sku_type, test_queue, test_set, test_trigger_next_model, test_keep_looping are not set")
+    #     exit(1)
 
     queue = get_test_queue()
+    model_list = list(queue.models)
+    for test_model_name in model_list:
 
-    # sku_override = get_sku_override()
-    # if sku_override is None:
-    #     check_override = False
-
-    # if test_trigger_next_model == "true":
-    #     set_next_trigger_model(queue)
-    # print values of all above variables
-    logger.info(f"test_subscription_id: {queue['subscription']}")
-    logger.info(f"test_resource_group: {queue['subscription']}")
-    logger.info(f"test_workspace_name: {queue['workspace']}")
-    logger.info(f"test_model_name: {test_model_name}")
-    logger.info(f"test_sku_type: {test_sku_type}")
-    logger.info(f"test_registry: {queue['registry']}")
-    logger.info(f"test_trigger_next_model: {test_trigger_next_model}")
-    logger.info(f"test_queue: {test_queue}")
-    logger.info(f"test_set: {test_set}")
-    logger.info(f"Here is my test model name : {test_model_name}")
-    try:
-        credential = DefaultAzureCredential()
-        credential.get_token("https://management.azure.com/.default")
-    except Exception as ex:
-        # Fall back to InteractiveBrowserCredential in case DefaultAzureCredential not work
-        credential = InteractiveBrowserCredential()
-    logger.info(f"workspace_name : {queue.workspace}")
-    try:
-        workspace_ml_client = MLClient.from_config(credential=credential)
-    except:
-        workspace_ml_client = MLClient(
-            credential=credential,
+        # sku_override = get_sku_override()
+        # if sku_override is None:
+        #     check_override = False
+    
+        # if test_trigger_next_model == "true":
+        #     set_next_trigger_model(queue)
+        # print values of all above variables
+        logger.info(f"test_subscription_id: {queue['subscription']}")
+        logger.info(f"test_resource_group: {queue['subscription']}")
+        logger.info(f"test_workspace_name: {queue['workspace']}")
+        logger.info(f"test_model_name: {test_model_name}")
+        logger.info(f"test_sku_type: {test_sku_type}")
+        logger.info(f"test_registry: {queue['registry']}")
+        logger.info(f"test_trigger_next_model: {test_trigger_next_model}")
+        logger.info(f"test_queue: {test_queue}")
+        logger.info(f"test_set: {test_set}")
+        logger.info(f"Here is my test model name : {test_model_name}")
+        try:
+            credential = DefaultAzureCredential()
+            credential.get_token("https://management.azure.com/.default")
+        except Exception as ex:
+            # Fall back to InteractiveBrowserCredential in case DefaultAzureCredential not work
+            credential = InteractiveBrowserCredential()
+        logger.info(f"workspace_name : {queue.workspace}")
+        try:
+            workspace_ml_client = MLClient.from_config(credential=credential)
+        except:
+            workspace_ml_client = MLClient(
+                credential=credential,
+                subscription_id=queue.subscription,
+                resource_group_name=queue.resource_group,
+                workspace_name=queue.workspace
+            )
+        ws = Workspace(
             subscription_id=queue.subscription,
-            resource_group_name=queue.resource_group,
+            resource_group=queue.resource_group,
             workspace_name=queue.workspace
         )
-    ws = Workspace(
-        subscription_id=queue.subscription,
-        resource_group=queue.resource_group,
-        workspace_name=queue.workspace
-    )
-    registry_ml_client = MLClient(
-        credential=credential,
-        registry_name="azureml-preview-test1"
-    )
-    azureml_registry = MLClient(credential, registry_name="azureml")
+        registry_ml_client = MLClient(
+            credential=credential,
+            registry_name="azureml-preview-test1"
+        )
+        azureml_registry = MLClient(credential, registry_name="azureml")
+        
+        azureml_meta_registry = MLClient(credential, registry_name="azureml-meta")
+        mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
+        if "lama" in test_model_name:
+            a = test_model_name.index('/')+1
+            model=test_model_name[a:]
+            model_detail = ModelDetail(workspace_ml_client=azureml_meta_registry)
+            foundation_model = model_detail.get_model_detail(test_model_name=model)
+            computelist = foundation_model.properties.get(
+            "evaluation-recommended-sku", "donotdelete-DS4v2")
+        else:
+            model_detail = ModelDetail(workspace_ml_client=azureml_registry)
+            foundation_model = model_detail.get_model_detail(test_model_name=test_model_name)
+            computelist = foundation_model.properties.get(
+            "evaluation-recommended-sku", "donotdelete-DS4v2")
+        if "," in computelist:
+            a = computelist.index(',')
+            COMPUTE = computelist[:a]
+        else:
+            COMPUTE = computelist
+        print("COMPUTE----------",COMPUTE)
+        compute_name="donotdelete-"+COMPUTE.replace("_", "-")
+        # compute_name=COMPUTE.replace("_", "-")
+        try:
+            _ = workspace_ml_client.compute.get(compute_name)
+            print("Found existing compute target.")
+        except ResourceNotFoundError:
+            print("Creating a new compute target...")
+            compute_config = AmlCompute(
+                name=compute_name,
+                type="amlcompute",
+                size=COMPUTE,
+                idle_time_before_scale_down=120,
+                min_instances=0,
+                max_instances=6,
+            )
+            workspace_ml_client.begin_create_or_update(compute_config).result()
+        # compute_target = create_or_get_compute_target(
+        #     workspace_ml_client, COMPUTE, instance_type=queue.instance_type)
     
-    azureml_meta_registry = MLClient(credential, registry_name="azureml-meta")
-    mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
-    if "lama" in test_model_name:
-        a = test_model_name.index('/')+1
-        model=test_model_name[a:]
-        model_detail = ModelDetail(workspace_ml_client=azureml_meta_registry)
-        foundation_model = model_detail.get_model_detail(test_model_name=model)
-        computelist = foundation_model.properties.get(
-        "evaluation-recommended-sku", "donotdelete-DS4v2")
-    else:
-        model_detail = ModelDetail(workspace_ml_client=azureml_registry)
-        foundation_model = model_detail.get_model_detail(test_model_name=test_model_name)
-        computelist = foundation_model.properties.get(
-        "evaluation-recommended-sku", "donotdelete-DS4v2")
-    if "," in computelist:
-        a = computelist.index(',')
-        COMPUTE = computelist[:a]
-    else:
-        COMPUTE = computelist
-    print("COMPUTE----------",COMPUTE)
-    compute_name="donotdelete-"+COMPUTE.replace("_", "-")
-    # compute_name=COMPUTE.replace("_", "-")
-    try:
-        _ = workspace_ml_client.compute.get(compute_name)
-        print("Found existing compute target.")
-    except ResourceNotFoundError:
-        print("Creating a new compute target...")
-        compute_config = AmlCompute(
-            name=compute_name,
-            type="amlcompute",
-            size=COMPUTE,
-            idle_time_before_scale_down=120,
-            min_instances=0,
-            max_instances=6,
-        )
-        workspace_ml_client.begin_create_or_update(compute_config).result()
-    # compute_target = create_or_get_compute_target(
-    #     workspace_ml_client, COMPUTE, instance_type=queue.instance_type)
-
-    # compute_target = create_or_get_compute_target(
-    #     ml_client=workspace_ml_client, compute=COMPUTE, instance_type=queue.instance_type)
-    task = HfTask(model_name=test_model_name).get_task(foundation_model=foundation_model)
-    print("Task--------------",task)
-    logger.info(f"Task is this : {task} for the model : {test_model_name}")
-    timestamp = str(int(time.time()))
-    exp_model_name = test_model_name.replace('/', '-')
-
-    registered_model_detail = ModelDetail(workspace_ml_client=workspace_ml_client)
-    registered_model = registered_model_detail.get_model_detail(test_model_name=test_model_name)
+        # compute_target = create_or_get_compute_target(
+        #     ml_client=workspace_ml_client, compute=COMPUTE, instance_type=queue.instance_type)
+        task = HfTask(model_name=test_model_name).get_task(foundation_model=foundation_model)
+        print("Task--------------",task)
+        logger.info(f"Task is this : {task} for the model : {test_model_name}")
+        timestamp = str(int(time.time()))
+        exp_model_name = test_model_name.replace('/', '-')
     
-
-    data_path = get_file_path(task=task)
-    input_column_names, label_column_name = get_dataset(task=task, data_path=data_path,
-                                                        latest_model=registered_model)
-    pipeline_task = get_pipeline_task(task)
- 
-    try:
-        pipeline_jobs = []
-        eval_experiment_name = f"{pipeline_task}-{exp_model_name}-evaluation-{timestamp}"
-        pipeline_object = evaluation_pipeline(
-            task=pipeline_task,
-            mlflow_model=Input(type=AssetTypes.MLFLOW_MODEL,
-                               path=f"{registered_model.id}"),
-            test_data=Input(type=AssetTypes.URI_FILE, path=data_path),
-            input_column_names=input_column_names,
-            label_column_name=label_column_name,
-            evaluation_file_path=Input(
-                type=AssetTypes.URI_FILE, path=f"./evaluation/{task}/eval_config.json"),
-            compute=compute_name,
-            #mlflow_model = f"{latest_model.id}",
-            #data_path = data_path
-        )
-        # don't reuse cached results from previous jobs
-        pipeline_object.settings.force_rerun = True
-        pipeline_object.settings.default_compute = compute_name
-
-        # set continue on step failure to False
-        pipeline_object.settings.continue_on_step_failure = False
-
-        pipeline_object.display_name = f"eval-{registered_model.name}-{timestamp}"
-        pipeline_job = workspace_ml_client.jobs.create_or_update(
-            pipeline_object, experiment_name=eval_experiment_name
-        )
-        # add model['name'] and pipeline_job.name as key value pairs to a dictionary
-        pipeline_jobs.append(
-            {"model_name": registered_model.name, "job_name": pipeline_job.name})
-        # wait for the pipeline job to complete
-        workspace_ml_client.jobs.stream(pipeline_job.name)
-        # return pipeline_jobs
-        metrics_df = MetricsCalaulator(
-            pipeline_jobs=pipeline_jobs, mlflow=mlflow, experiment_name=eval_experiment_name).display_metric()
-        logger.info(f"Evaluation result is this : {metrics_df}")
-    except Exception as ex:
-        _, _, exc_tb = sys.exc_info()
-        logger.error(f"The exception occured at this line no : {exc_tb.tb_lineno}" +
-                     f" the exception is this one : \n {ex}")
-        raise Exception(ex)
-    logger.info("Proceeding with inference and deployment")
-    # InferenceAndDeployment = ModelInferenceAndDeployemnt(
-    #     test_model_name=test_model_name.lower(),
-    #     workspace_ml_client=workspace_ml_client,
-    #     registry=queue.registry
-    # )
-    # InferenceAndDeployment.model_infernce_and_deployment(
-    #     instance_type=queue.instance_type,
-    #     task=task,
-    #     latest_model=registered_model
-    # )
+        registered_model_detail = ModelDetail(workspace_ml_client=workspace_ml_client)
+        registered_model = registered_model_detail.get_model_detail(test_model_name=test_model_name)
+        
+    
+        data_path = get_file_path(task=task)
+        input_column_names, label_column_name = get_dataset(task=task, data_path=data_path,
+                                                            latest_model=registered_model)
+        pipeline_task = get_pipeline_task(task)
+     
+        try:
+            pipeline_jobs = []
+            eval_experiment_name = f"{pipeline_task}-{exp_model_name}-evaluation-{timestamp}"
+            pipeline_object = evaluation_pipeline(
+                task=pipeline_task,
+                mlflow_model=Input(type=AssetTypes.MLFLOW_MODEL,
+                                   path=f"{registered_model.id}"),
+                test_data=Input(type=AssetTypes.URI_FILE, path=data_path),
+                input_column_names=input_column_names,
+                label_column_name=label_column_name,
+                evaluation_file_path=Input(
+                    type=AssetTypes.URI_FILE, path=f"./evaluation/{task}/eval_config.json"),
+                compute=compute_name,
+                #mlflow_model = f"{latest_model.id}",
+                #data_path = data_path
+            )
+            # don't reuse cached results from previous jobs
+            pipeline_object.settings.force_rerun = True
+            pipeline_object.settings.default_compute = compute_name
+    
+            # set continue on step failure to False
+            pipeline_object.settings.continue_on_step_failure = False
+    
+            pipeline_object.display_name = f"eval-{registered_model.name}-{timestamp}"
+            pipeline_job = workspace_ml_client.jobs.create_or_update(
+                pipeline_object, experiment_name=eval_experiment_name
+            )
+            # add model['name'] and pipeline_job.name as key value pairs to a dictionary
+            pipeline_jobs.append(
+                {"model_name": registered_model.name, "job_name": pipeline_job.name})
+            # wait for the pipeline job to complete
+            workspace_ml_client.jobs.stream(pipeline_job.name)
+            # return pipeline_jobs
+            metrics_df = MetricsCalaulator(
+                pipeline_jobs=pipeline_jobs, mlflow=mlflow, experiment_name=eval_experiment_name).display_metric()
+            logger.info(f"Evaluation result is this : {metrics_df}")
+        except Exception as ex:
+            _, _, exc_tb = sys.exc_info()
+            logger.error(f"The exception occured at this line no : {exc_tb.tb_lineno}" +
+                         f" the exception is this one : \n {ex}")
+            raise Exception(ex)
+        logger.info("Proceeding with inference and deployment")
+   
