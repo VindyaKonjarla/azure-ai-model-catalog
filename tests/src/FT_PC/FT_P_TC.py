a39abfd29a3e9ba77cbcae057df3589de4da7494
PoojaRam-12 <141908731+PoojaRam-12@users.noreply.github.com>
2023-12-07 20:21:32 +0530

    Update FT_P_TC.py

--- tests/src/FT_PC/FT_P_TC.py
+++ tests/src/FT_PC/FT_P_TC.py
@@ -67,32 +67,6 @@ def get_sku_override():
         return None
 
 
-# def set_next_trigger_model(queue):
-#     print("In set_next_trigger_model...")
-# # file the index of test_model_name in models list queue dictionary
-#     model_list = list(queue.models)
-#     #model_name_without_slash = test_model_name.replace('/', '-')
-#     check_mlflow_model = "MLFlow-FT-"+test_model_name+"-oss"
-#     index = model_list.index(check_mlflow_model)
-#     print("check_mlflow_model:", {check_mlflow_model})
-#     print("index of check_mlflow_model:", {index})
-#     #index = model_list.index(test_model_name)
-#     #index = model_list.index(test_model_name)
-#     print(f"index of {test_model_name} in queue: {index}")
-# # if index is not the last element in the list, get the next element in the list
-#     if index < len(model_list) - 1:
-#         next_model = model_list[index + 1]
-#     else:
-#         if (test_keep_looping == "true"):
-#             next_model = queue[0]
-#         else:
-#             print("::warning:: finishing the queue")
-#             next_model = ""
-# # write the next model to github step output
-#     with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-#         print(f'NEXT_MODEL={next_model}')
-#         print(f'NEXT_MODEL={next_model}', file=fh)
-
 
 
 def run_azure_ml_job(code, command_to_run, environment, compute, environment_variables):
@@ -205,73 +179,6 @@ def get_training_and_optimization_parameters(foundation_model):
 
 
 
-# def create_or_get_aml_compute(workspace_ml_client, compute_cluster, compute_cluster_size, computes_allow_list=None):
-#     try:
-#         compute = workspace_ml_client.compute.get(compute_cluster)
-#         print(f"The compute cluster '{compute_cluster}' already exists! Reusing it for the current run")
-#     except Exception as ex:
-#         print(f"Looks like the compute cluster '{compute_cluster}' doesn't exist. Creating a new one with compute size '{compute_cluster_size}'!")
-
-#         # Define a list of VM sizes that are not supported for finetuning
-#         unsupported_gpu_vm_list = ["standard_nc6", "standard_nc12", "standard_nc24", "standard_nc24r"]
-
-#         try:
-#             print("Attempt #1 - Trying to create a dedicated compute")
-#             tier = "Dedicated"
-#             if compute_cluster_size.lower() in unsupported_gpu_vm_list:
-#                 raise ValueError(f"VM size '{compute_cluster_size}' is not supported for finetuning.")
-#         except ValueError as e:
-#             print(e)
-#             raise
-
-#         try:
-#             print("Attempt #2 - Trying to create a low priority compute. Since this is a low priority compute, the job could get pre-empted before completion.")
-#             tier = "LowPriority"
-#             if compute_cluster_size.lower() in unsupported_gpu_vm_list:
-#                 raise ValueError(f"VM size '{compute_cluster_size}' is not supported for finetuning.")
-#         except ValueError as e:
-#             print(e)
-#             raise
-
-#         # Provision the compute
-#         compute = AmlCompute(
-#             name=compute_cluster,
-#             size=compute_cluster_size,
-#             tier=tier,
-#             max_instances=2,  # For multi-node training, set this to an integer value more than 1
-#         )
-#         workspace_ml_client.compute.begin_create_or_update(compute).wait()
-
-#     # Sanity check on the created compute
-#     compute = workspace_ml_client.compute.get(compute_cluster)
-
-#     if compute.provisioning_state.lower() == "failed":
-#         raise ValueError(f"Provisioning failed. Compute '{compute_cluster}' is in a failed state. Please try creating a different compute.")
-
-#     if computes_allow_list is not None:
-#         computes_allow_list_lower_case = [x.lower() for x in computes_allow_list]
-#         if compute.size.lower() not in computes_allow_list_lower_case:
-#             raise ValueError(f"VM size '{compute.size}' is not in the allow-listed computes for finetuning.")
-    
-#     # Determine the number of GPUs in a single node of the selected 'compute_cluster_size' compute
-#     gpu_count_found = False
-#     workspace_compute_sku_list = workspace_ml_client.compute.list_sizes()
-#     available_sku_sizes = []
-
-#     for compute_sku in workspace_compute_sku_list:
-#         available_sku_sizes.append(compute_sku.name)
-#         if compute_sku.name.lower() == compute.size.lower():
-#             gpus_per_node = compute_sku.gpus
-#             gpu_count_found = True
-
-#     # If the GPU count is not found, print an error
-#     if gpu_count_found:
-#         print(f"Number of GPUs in compute '{compute_cluster}': {gpus_per_node}")
-#     else:
-#         raise ValueError(f"Number of GPUs in compute '{compute_cluster}' not found. Available skus are: {available_sku_sizes}. This should not happen. Please check the selected compute cluster: {compute_cluster} and try again.")
-    
-#     return compute, gpus_per_node, compute_cluster
-
 
 def create_or_get_aml_compute(workspace_ml_client, compute_cluster, compute_cluster_size):
     try:
@@ -321,9 +228,6 @@ def create_or_get_aml_compute(workspace_ml_client, compute_cluster, compute_clus
         raise ValueError(f"Number of GPUs in compute '{compute_cluster}' not found. Available skus are: {available_sku_sizes}. This should not happen. Please check the selected compute cluster: {compute_cluster} and try again.")
     
     return compute, gpus_per_node, compute_cluster
-
-
-
 
 
 
@@ -495,26 +399,16 @@ if __name__ == "__main__":
     experiment_name = "oss-text-classification-"+ test_model_name
     print("Experiment name is:", {experiment_name})
 
-
-    # Define the compute cluster name and size
-    # compute_cluster = "finetuning-recommended-sku"
-    # compute_cluster_size = "fine_tune_sku"
-
     
     compute_cluster_size = fine_tune_sku
     compute_cluster = "donotdelete-" + compute_cluster_size.replace('_', '-')
     print("Modified compute_cluster_size:", compute_cluster_size)
     print("Modified compute_cluster_size:", {compute_cluster})
 
-    
-    # # Optional: Define a list of allowed compute sizes (if any)
-    # computes_allow_list = ["standard_nc6s_v3", "standard_nc12s_v2","standard_nc24s_v3","standard_NC24rs_v3","Standard_NC12s_v3"]
+
     
     # Call the function
     compute, gpus_per_node, compute_cluster = create_or_get_aml_compute(workspace_ml_client, compute_cluster, compute_cluster_size)
-
-
-
 
     
     # compute = create_or_get_compute_target(workspace_ml_client, queue.compute)
