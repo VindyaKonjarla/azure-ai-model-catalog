ca1e294dec1c60ad758258a49a6a15d1532bad56
PoojaRam-12 <141908731+PoojaRam-12@users.noreply.github.com>
2023-12-21 13:23:49 +0530

    Update FT_P_TG_HF.py

--- tests/src/FT_PC/FT_P_TG_HF.py
+++ tests/src/FT_PC/FT_P_TG_HF.py
@@ -226,35 +226,35 @@ def create_and_run_azure_ml_pipeline(
         name="text_generation_pipeline_for_oss", label="latest"
     )
 
-    def register_model_to_workspace(
-        workspace_ml_client, pipeline_job, test_model_name, timestamp
-    ):
-        print("Registering the model inside loop...")
-        model_path_from_job = "azureml://jobs/{0}/outputs/{1}".format(
-            pipeline_job.name, "trained_model"
-        )
-        finetuned_model_name = (
-            "FT-TG-" + str(test_model_name) + "-hf"
-        )
-        finetuned_model_name = finetuned_model_name.replace("/", "-")
-        print("The Finetuned model name inside loop:", finetuned_model_name)
+    # def register_model_to_workspace(
+    #     workspace_ml_client, pipeline_job, test_model_name, timestamp
+    # ):
+    #     print("Registering the model inside loop...")
+    #     model_path_from_job = "azureml://jobs/{0}/outputs/{1}".format(
+    #         pipeline_job.name, "trained_model"
+    #     )
+    #     finetuned_model_name = (
+    #         "FT-TG-" + str(test_model_name) + "-hf"
+    #     )
+    #     finetuned_model_name = finetuned_model_name.replace("/", "-")
+    #     print("The Finetuned model name inside loop:", finetuned_model_name)
 
-        print("Path to register model inside loop: ", model_path_from_job)
+    #     print("Path to register model inside loop: ", model_path_from_job)
 
-        prepare_to_register_model = Model(
-        path=model_path_from_job,
-        type=AssetTypes.MLFLOW_MODEL,
-        name=finetuned_model_name,
-        version=timestamp,  # use timestamp as version to avoid version conflict
-        description= test_model_name + " fine tuned model for emotion detection",
-        )
-        print("prepare to register model inside loop:", prepare_to_register_model)
+    #     prepare_to_register_model = Model(
+    #     path=model_path_from_job,
+    #     type=AssetTypes.MLFLOW_MODEL,
+    #     name=finetuned_model_name,
+    #     version=timestamp,  # use timestamp as version to avoid version conflict
+    #     description= test_model_name + " fine tuned model for emotion detection",
+    #     )
+    #     print("prepare to register model inside loop:", prepare_to_register_model)
     
-        registered_model = workspace_ml_client.models.create_or_update(
-            prepare_to_register_model
-        )
+    #     registered_model = workspace_ml_client.models.create_or_update(
+    #         prepare_to_register_model
+    #     )
     
-        print("Registered model inside loop: \n", registered_model)
+    #     print("Registered model inside loop: \n", registered_model)
         
     # Model Training and Pipeline Setup
     @pipeline()
@@ -302,9 +302,9 @@ def create_and_run_azure_ml_pipeline(
     print("Pipeline Job Completed")
 
     # Call the model registration function
-    register_model_to_workspace(
-        workspace_ml_client, pipeline_job, test_model_name, timestamp
-    )
+    # register_model_to_workspace(
+    #     workspace_ml_client, pipeline_job, test_model_name, timestamp
+    # )
     return pipeline_job
     
     
