ec26b97c9045d71e1cd2dd7a8b2679d51f5e33c2
pra1405 <141804228+pra1405@users.noreply.github.com>
2023-12-27 10:59:47 +0530

    Update generic_initial_automation1.py

--- tests/src/automation_for_constant_library/generic_initial_automation1.py
+++ tests/src/automation_for_constant_library/generic_initial_automation1.py
@@ -82,7 +82,7 @@ def set_next_trigger_model(queue):
     if check_mlflow_model in model_list:
         index = model_list.index(check_mlflow_model)
     else:
-        index = model_list.index("MLFlow-MP-"+test_model_name)
+        index = model_list.index(test_model_name)
     #index = model_list.index(test_model_name)
     logger.info(f"index of {test_model_name} in queue: {index}")
 # if index is not the last element in the list, get the next element in the list
